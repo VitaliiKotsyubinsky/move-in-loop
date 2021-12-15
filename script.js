a9711833e42c35adae50b5bec5111934edfe3e15
VitaliiKotsyubinsky <magnoom2010@gmail.com>
2021-12-15 21:08:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const actor = {
+    x: 0,
+    y: 0,
+    size: 50,
+    el: document.getElementById('actor'),
+}
+
+main()
+
+async function main() {
+    while (true) {
+        await gameLoop()
+    }
+
+}
+
+async function gameLoop() {
+    if (actor.x < innerWidth / 2) {
+        await moveRight()
+    } else {
+        await moveDown()
+    }
+
+}
+
+async function moveRight() {
+    actor.x += actor.size
+    render()
+    await sleep(1)
+}
+async function moveDown() {
+    actor.y += actor.size
+    render()
+    await sleep(1)
+}
+
+
+function sleep(delay) {
+    return new Promise(resolve => setTimeout(resolve, delay * 1000))
+}
+
+function render() {
+    actor.el.style.left = actor.x + 'px'
+    actor.el.style.top = actor.y + 'px'
+}
